22c4f1c40379d70b0e61d50cc20fd5dc487beff8
Gamer64 <76565986+Gamer64ytb@users.noreply.github.com>
2024-07-31 19:40:06 +0200

    Only use sample shading on vk_graphics_pipeline

--- src/video_core/renderer_vulkan/renderer_vulkan.cpp
+++ src/video_core/renderer_vulkan/renderer_vulkan.cpp
@@ -311,7 +311,7 @@ void RendererVulkan::BuildPipelines() {
 
     const vk::PipelineMultisampleStateCreateInfo multisampling = {
         .rasterizationSamples = vk::SampleCountFlagBits::e1,
-        .sampleShadingEnable = Settings::values.use_sample_shading.GetValue(),
+        .sampleShadingEnable = false,
     };
 
     const vk::PipelineColorBlendAttachmentState colorblend_attachment = {
